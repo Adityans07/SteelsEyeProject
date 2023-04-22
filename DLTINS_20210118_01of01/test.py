ba263ca5068493c7273bfe3be706677061d0ddeb
Adityans07 <79587694+Adityans07@users.noreply.github.com>
2023-04-22 16:44:05 +0530

    Add files via upload

---
+++ DLTINS_20210118_01of01/test.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        Id = "D4568877"
+        self.assertIsNotNone(Id)  # add assertion here
+
+    def isNotNone(self):
+        text = "SomeText"
+        self.assertIsNotNone(text)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
